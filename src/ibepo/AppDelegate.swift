360c46ccd26273737a6487ed4664a1ad08005fcf
Steve Gigou <steve@account.gigou.fr>
2020-04-30 15:52:33 +0200

    Remove storyboards

--- src/ibepo/AppDelegate.swift
+++ src/ibepo/AppDelegate.swift
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    // TODO: Replace stub with real home view controller
+    let homeViewController = UIViewController()
+    homeViewController.view.backgroundColor = UIColor.red
+    window?.rootViewController = homeViewController
+    window?.makeKeyAndVisible()
     return true
   }
 
