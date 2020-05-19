edfd9e270dcb16fa3129e58b5f1e17695413fc80
Steve Gigou <steve@account.gigou.fr>
2020-05-19 10:18:14 +0200

    Group UI files by theme

---
+++ src/keyboard/UI/Autocorrect/AutocorrectViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AutocorrectViewController.swift
+//  ibepo
+//
+//  Created by Steve Gigou on 2020-05-19.
+//  Copyright © 2020 Novesoft. All rights reserved.
+//
+
+import UIKit
+
+class AutocorrectViewController: UIViewController {
+  
+}
