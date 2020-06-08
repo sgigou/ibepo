66486bfa50e85761d8632c7932c5385ce1f3b74b
Steve Gigou <steve@account.gigou.fr>
2020-06-08 14:53:24 +0200

    Words type

--- src/keyboard/Services/AutoCapitalizer.swift
+++ src/keyboard/Services/AutoCapitalizer.swift
@@ -13,9 +13,18 @@ class AutoCapitalizer {
     switch type {
     case .allCharacters:
       return true
+    case .words:
+      return calculateForWordsType()
     default:
       return false
     }
+  }
+  
+  private func calculateForWordsType() -> Bool {
+    guard let lastCharacter = KeyboardSettings.shared.textDocumentProxyAnalyzer.findContext().last else {
+      return true
+    }
+    return lastCharacter.isWhitespace || lastCharacter.isNewline
   }
   
 }
