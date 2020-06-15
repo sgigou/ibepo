3a56c6c0c3eaf5159db908f20425a9eeeb2f0eea
Steve Gigou <steve@account.gigou.fr>
2020-06-15 14:01:26 +0200

    Close keyboard after enter press

---
+++ src/ibepo/UI/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  ibepo
+//
+//  Created by Steve Gigou on 2020-06-15.
+//  Copyright © 2020 Novesoft. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - MainViewController
+
+class MainViewController: UIViewController {
+
+}
+
+
+// MARK: - UITextFieldDelegate
+
+extension MainViewController: UITextFieldDelegate {
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
+  }
+  
+}
