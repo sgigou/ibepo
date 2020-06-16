ee3c91fca8f6f235aad97467a7c69d5ff1874bee
Steve Gigou <steve@account.gigou.fr>
2020-06-16 08:38:33 +0200

    Gestures

--- src/ibepo/UI/TutorialPageViewController.swift
+++ src/ibepo/UI/TutorialPageViewController.swift
@@ -36,7 +36,7 @@ class TutorialPageViewController: UIPageViewController {
   }
   
   private func loadPages() {
-    for pageIndex in 1...4 {
+    for pageIndex in 1...5 {
       if let page = storyboard?.instantiateViewController(withIdentifier: "page\(pageIndex)") {
         pages.append(page)
       }
