37048a9442350f06587ad6be1215c7f2451d3c76
Steve Gigou <steve@account.gigou.fr>
2020-09-26 15:38:51 +0200

    Toolbar on keyboard and auto resize

--- src/ibepo/UI/EditorViewController.swift
+++ src/ibepo/UI/EditorViewController.swift
@@ -16,34 +16,54 @@ class EditorViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    observeKeyboardNotifications()
     textView.delegate = self
     textView.inputAccessoryView = createToolbar()
   }
 
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    navigationController?.setToolbarHidden(false, animated: animated)
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    textView.becomeFirstResponder()
   }
 
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
-    navigationController?.setToolbarHidden(true, animated: animated)
+    textView.resignFirstResponder()
+    NotificationCenter.default.removeObserver(self)
   }
 
   // MARK: Drawing
 
   func createToolbar() -> UIToolbar {
-    var toolBar = UIToolbar()
-    toolBar.barStyle = UIBarStyle.default
-    toolBar.isTranslucent = true
-        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
-    var doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: nil)
-    var cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: nil)
-    var spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
-    toolBar.isUserInteractionEnabled = true
-        toolBar.sizeToFit()
+    let toolBar = UIToolbar()
+    let deleteButton = UIBarButtonItem(image: UIImage(named: "trash-2"), style: .plain, target: nil, action: nil)
+    deleteButton.tintColor = .systemRed
+    let pasteButton = UIBarButtonItem(image: UIImage(named: "clipboard"), style: .plain, target: nil, action: nil)
+    pasteButton.tintColor = .systemRed
+    let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    let copyButton = UIBarButtonItem(image: UIImage(named: "copy"), style: .plain, target: nil, action: nil)
+    toolBar.setItems([deleteButton, pasteButton, spaceButton, copyButton], animated: false)
+    toolBar.sizeToFit()
     return toolBar
+  }
+
+  // MARK: Keyboard
+
+  private func observeKeyboardNotifications() {
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardFrameDidChange(_:)), name: UIResponder.keyboardDidChangeFrameNotification, object: nil)
+  }
+
+  @objc func keyboardFrameDidChange(_ notification: NSNotification) {
+    guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size else { return }
+    var safeAreaBottom: CGFloat = 0.0
+    if #available(iOS 11.0, *) {
+      safeAreaBottom = view.safeAreaInsets.bottom
+    }
+    let bottomInset = keyboardSize.height - safeAreaBottom
+    let contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: bottomInset, right: 0.0)
+    textView.contentInset = contentInset
+    textView.scrollIndicatorInsets = contentInset
+    textView.scrollRangeToVisible(textView.selectedRange)
   }
 
 }
