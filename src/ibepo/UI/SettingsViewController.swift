69f6643d1d612c1de2f7bd39ea0c90250ea19f1c
Steve Gigou <steve@account.gigou.fr>
2020-11-29 09:28:45 +0100

    Settings table view

---
+++ src/ibepo/UI/SettingsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsViewController.swift
+//  ibepo
+//
+//  Created by Steve Gigou on 29/11/2020.
+//  Copyright © 2020 Novesoft. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+  // MARK: Table view data source
+
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 0
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+
+}
